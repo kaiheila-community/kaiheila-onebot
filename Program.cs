025c116e68641f25e39be1a53e423d592c3366b2
Il Harper <afanyiyu@hotmail.com>
2020-11-11 23:16:48 +0800

    feat: Add Generic Host

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Kaiheila.Cqhttp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+
+            host.Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+
+                });
+    }
+}
