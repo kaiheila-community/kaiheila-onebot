f71eff70673a0cd2c44a4219b6c2c02492fb7aee
Il Harper <afanyiyu@hotmail.com>
2020-11-20 22:01:52 +0800

    feat: Add CommunicationHelper

---
+++ src/Utils/CommunicationHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace Kaiheila.Cqhttp.Utils
+{
+    public static class CommunicationHelper
+    {
+        public static void SetStatusCode(this HttpResponse httpResponse, HttpStatusCode httpStatusCode) =>
+            httpResponse.StatusCode = (int)httpStatusCode;
+    }
+}
