d0de8ae8bdea91c1e5f1575d0d0ba12e34c886a5
Il Harper <afanyiyu@hotmail.com>
2020-11-22 01:12:35 +0800

    feat: Add Process Method in CqEventHandler

--- src/Cq/Handlers/CqEventHandler.cs
+++ src/Cq/Handlers/CqEventHandler.cs
@@ -1,6 +1,9 @@
-﻿using System.Reactive.Subjects;
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Kaiheila.Cqhttp.Cq.Events;
+using Kaiheila.Cqhttp.Kh;
 using Kaiheila.Cqhttp.Storage;
+using Kaiheila.Events;
 using Microsoft.Extensions.Logging;
 
 namespace Kaiheila.Cqhttp.Cq.Handlers
@@ -16,18 +19,29 @@ namespace Kaiheila.Cqhttp.Cq.Handlers
         /// <param name="logger">CQHTTP任务处理器日志记录器。</param>
         /// <param name="configHelper">提供访问应用配置能力的帮助类型。</param>
         public CqEventHandler(
+            KhHost khHost,
             ILogger<CqActionHandler> logger,
             ConfigHelper configHelper)
         {
+            _khHost = khHost;
             _logger = logger;
             _configHelper = configHelper;
+
+            khHost.Bot.Event.Select(Process).Subscribe(Event);
         }
 
         #region Event
 
+        private CqEventBase Process(KhEventBase khEvent)
+        {
+            return new CqEventBase();
+        }
+
         public Subject<CqEventBase> Event = new Subject<CqEventBase>();
 
         #endregion
+
+        private readonly KhHost _khHost;
 
         /// <summary>
         /// CQHTTP事件处理器日志记录器。
