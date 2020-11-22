eff3f09f7746d04b504cb11284f41146794cfed3
Il Harper <afanyiyu@hotmail.com>
2020-11-22 11:10:55 +0800

    test: Add CqControllerBasedOnCqControllerBase

---
+++ test/Cq/Controllers/CqControllers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Kaiheila.Cqhttp.Cq.Controllers;
+using Xunit;
+
+namespace Kaiheila.Cqhttp.Test.Cq.Controllers
+{
+    public class CqControllers
+    {
+        [Fact]
+        public void CqControllerBasedOnCqControllerBase()
+        {
+            foreach (Type type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x =>
+                x.GetTypes()
+                    .Where(type => Attribute.GetCustomAttribute(type, typeof(CqControllerAttribute)) is not null)))
+                Assert.Equal(type.BaseType, typeof(CqControllerBase));
+        }
+    }
+}
