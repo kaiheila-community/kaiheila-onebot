b5c3a4aad6fff94daed13da4cffb01bdb3f3e661
Il Harper <afanyiyu@hotmail.com>
2020-11-22 10:49:25 +0800

    feat: KhHost Class Update

--- src/Kh/KhHost.cs
+++ src/Kh/KhHost.cs
@@ -1,7 +1,13 @@
 ﻿namespace Kaiheila.Cqhttp.Kh
 {
+    /// <summary>
+    /// Kaiheila主机。
+    /// </summary>
     public class KhHost
     {
+        /// <summary>
+        /// Kaiheila机器人。
+        /// </summary>
         public readonly Bot Bot;
     }
 }
