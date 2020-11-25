dc0ff310cf426b3698c316bec433ef943fa288be
Il Harper <afanyiyu@hotmail.com>
2020-11-25 22:05:37 +0800

    feat: Add CqCode Class

---
+++ src/Cq/Code/CqCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kaiheila.Cqhttp.Cq.Code
+{
+    public class CqCode
+    {
+        public Dictionary<string, string> Params = new Dictionary<string, string>();
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    sealed class CqCodeTypeAttribute : Attribute
+    {
+        public CqCodeTypeAttribute(string type) => Type = type;
+
+        public readonly string Type;
+    }
+}
