49853a413f320ea687428acc6fac4638099e7b6e
Il Harper <afanyiyu@hotmail.com>
2020-11-25 22:14:42 +0800

    test: Add CqCodeTypesBasedOnCqCode

---
+++ test/Cq/Code/CqCodes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Kaiheila.Cqhttp.Cq.Code;
+using Xunit;
+
+namespace Kaiheila.Cqhttp.Test.Cq.Code
+{
+    public class CqCodes
+    {
+        [Fact]
+        public void CqCodeTypesBasedOnCqCode()
+        {
+            foreach (Type type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x =>
+                x.GetTypes()
+                    .Where(type => Attribute.GetCustomAttribute(type, typeof(CqCodeTypeAttribute)) is not null)))
+                Assert.Equal(typeof(CqCode), type.BaseType);
+        }
+    }
+}
