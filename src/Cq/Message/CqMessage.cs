f408f65abbea29c06d2733aae17f0eafaefa1036
Il Harper <afanyiyu@hotmail.com>
2020-11-28 17:50:12 +0800

    feat: Add CqMessage

---
+++ src/Cq/Message/CqMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Kaiheila.Cqhttp.Cq.Code;
+
+namespace Kaiheila.Cqhttp.Cq.Message
+{
+    public class CqMessage
+    {
+        #region Data
+
+        public List<CqCode> CodeList = new List<CqCode>();
+
+        #endregion
+
+        internal CqMessage()
+        {
+
+        }
+    }
+}
