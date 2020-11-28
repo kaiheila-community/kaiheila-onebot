cfcbee0da27775868ae72d923e9964a6a609b32d
Il Harper <afanyiyu@hotmail.com>
2020-11-28 19:39:06 +0800

    feat: Add CqCodeImage

---
+++ src/Cq/Code/CqCodeImage.cs
@@ -0,0 +1,19 @@
+﻿namespace Kaiheila.Cqhttp.Cq.Code
+{
+    [CqCodeType("image")]
+    public class CqCodeImage : CqCode
+    {
+        public CqCodeImage(CqCode cqCode)
+        {
+            Params = cqCode.Params;
+            File = Params["file"];
+        }
+
+        public CqCodeImage(string file)
+        {
+            File = file;
+        }
+
+        public readonly string File;
+    }
+}
