5e2302f9539d4af7c49ae6ec32e762011c87bec0
Il Harper <afanyiyu@hotmail.com>
2020-11-28 19:50:09 +0800

    test: Add CqMessages

---
+++ test/Cq/Message/CqMessages.cs
@@ -0,0 +1,70 @@
+﻿using Kaiheila.Cqhttp.Cq.Code;
+using Kaiheila.Cqhttp.Cq.Message;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Kaiheila.Cqhttp.Test.Cq.Message
+{
+    public class CqMessages
+    {
+        #region Data
+
+        private const string StringToken = "[CQ:face,id=178]看看我刚拍的照片[CQ:image,file=123.jpg]";
+
+        // ReSharper disable once InconsistentNaming
+        private readonly JArray ArrayToken = JArray.FromObject(
+            new object[]
+            {
+                new
+                {
+                    type = "face",
+                    data = new
+                    {
+                        id = "178"
+                    }
+                },
+                new
+                {
+                    type = "text",
+                    data = new
+                    {
+                        text = "看看我刚拍的照片"
+                    }
+                },
+                new
+                {
+                    type = "image",
+                    data = new
+                    {
+                        file = "123.jpg"
+                    }
+                }
+            });
+
+        #endregion
+
+        private static void ParseMessage(JToken token)
+        {
+            CqMessageHost cqMessageHost = new CqMessageHost(new CqCodeHost(new Logger<CqCodeHost>(new LoggerFactory())));
+
+            CqMessage message = cqMessageHost.Parse(token);
+
+            Assert.Equal(3, message.CodeList.Count);
+
+            Assert.IsType<CqCodeFace>(message.CodeList[0]);
+            Assert.IsType<CqCodeText>(message.CodeList[1]);
+            Assert.IsType<CqCodeImage>(message.CodeList[2]);
+
+            Assert.Equal(178, ((CqCodeFace) message.CodeList[0]).Id);
+            Assert.Equal("看看我刚拍的照片", ((CqCodeText) message.CodeList[1]).Text);
+            Assert.Equal("123.jpg", ((CqCodeImage) message.CodeList[2]).File);
+        }
+
+        [Fact]
+        public void ParseStringMessage() => ParseMessage(JValue.CreateString(StringToken));
+
+        [Fact]
+        public void ParseArrayMessage() => ParseMessage(ArrayToken);
+    }
+}
