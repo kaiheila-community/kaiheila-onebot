5db1ee4c857a8a89567df22d76c34260ef92d891
Il Harper <afanyiyu@hotmail.com>
2020-11-28 19:31:56 +0800

    feat: Add CqCodeFace

---
+++ src/Cq/Code/CqCodeFace.cs
@@ -0,0 +1,19 @@
+﻿namespace Kaiheila.Cqhttp.Cq.Code
+{
+    [CqCodeType("face")]
+    public class CqCodeFace : CqCode
+    {
+        public CqCodeFace(CqCode cqCode)
+        {
+            Params = cqCode.Params;
+            Id = ushort.Parse(Params["id"]);
+        }
+
+        public CqCodeFace(ushort id)
+        {
+            Id = id;
+        }
+
+        public readonly ushort Id;
+    }
+}
