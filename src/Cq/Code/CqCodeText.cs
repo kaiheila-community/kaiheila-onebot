0e75473861a3a70a4bc814f08210302b90e6f6d7
Il Harper <afanyiyu@hotmail.com>
2020-11-28 17:49:34 +0800

    feat: Add CqCodeText

---
+++ src/Cq/Code/CqCodeText.cs
@@ -0,0 +1,19 @@
+﻿namespace Kaiheila.Cqhttp.Cq.Code
+{
+    [CqCodeType("text")]
+    public class CqCodeText : CqCode
+    {
+        public CqCodeText(CqCode cqCode)
+        {
+            Params = cqCode.Params;
+            Text = Params["text"];
+        }
+
+        public CqCodeText(string text)
+        {
+            Text = text;
+        }
+
+        public readonly string Text;
+    }
+}
