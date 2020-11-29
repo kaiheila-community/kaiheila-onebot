6a356598118a458d0158bbeacc193ec993842c7e
Il Harper <afanyiyu@hotmail.com>
2020-11-29 20:28:05 +0800

    feat: CqActionHandler Update

--- src/Cq/Handlers/CqActionHandler.cs
+++ src/Cq/Handlers/CqActionHandler.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Kaiheila.Cqhttp.Cq.Controllers;
 using Kaiheila.Cqhttp.Kh;
