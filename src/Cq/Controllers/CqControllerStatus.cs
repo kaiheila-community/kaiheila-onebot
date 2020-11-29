b63b5dab9b9cf8ff756bf7aa362f8849f1f290e5
Il Harper <afanyiyu@hotmail.com>
2020-11-29 20:49:19 +0800

    feat: Add CqControllerStatus

---
+++ src/Cq/Controllers/CqControllerStatus.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace Kaiheila.Cqhttp.Cq.Controllers
+{
+    [CqController("get_status")]
+    public class CqControllerGetStatus : CqControllerBase
+    {
+        public CqControllerGetStatus(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload) =>
+            JToken.FromObject(new
+            {
+                online = true,
+                good = true
+            });
+    }
+
+    [CqController("get_version_info")]
+    public class CqControllerGetVersionInfo : CqControllerBase
+    {
+        public CqControllerGetVersionInfo(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload) => 
+            JToken.FromObject(new
+            {
+                app_name = "kaiheila-cqhttp",
+                app_version = Assembly.GetExecutingAssembly().GetName().Version?.ToString(),
+                protocol_version = "v11"
+            });
+    }
+
+    [CqController("set_restart")]
+    public class CqControllerSetRestart : CqControllerBase
+    {
+        public CqControllerSetRestart(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload) => JToken.FromObject(new{});
+    }
+
+    [CqController("clean_cache")]
+    public class CqControllerCleanCache : CqControllerBase
+    {
+        public CqControllerCleanCache(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload) => JToken.FromObject(new { });
+    }
+}
