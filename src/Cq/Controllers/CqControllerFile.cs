1f45e0502cda3c8c4133b25825c7d4eaac1569dd
Il Harper <afanyiyu@hotmail.com>
2020-11-29 20:40:54 +0800

    feat: Add CqControllerFile

---
+++ src/Cq/Controllers/CqControllerFile.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Kaiheila.Cqhttp.Cq.Controllers
+{
+    [CqController("can_send_image")]
+    public class CqControllerCanSendImage : CqControllerBase
+    {
+        public CqControllerCanSendImage(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload) =>
+            JToken.FromObject(new
+            {
+                yes = true
+            });
+    }
+
+    [CqController("can_send_record")]
+    public class CqControllerCanSendRecord : CqControllerBase
+    {
+        public CqControllerCanSendRecord(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload) =>
+            JToken.FromObject(new
+            {
+                yes = false
+            });
+    }
+}
