1d44ed25e550f9c8c9ae693270bc7cd59cd434e0
Il Harper <afanyiyu@hotmail.com>
2020-12-01 14:49:13 +0800

    feat: Add CqControllerSendGroupMessage

---
+++ src/Cq/Controllers/CqControllerSendMessage.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Kaiheila.Cqhttp.Cq.Controllers
+{
+    [CqController("send_private_msg")]
+    public class CqControllerSendPrivateMessage : CqControllerBase
+    {
+        public CqControllerSendPrivateMessage(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    [CqController("send_group_msg")]
+    public class CqControllerSendGroupMessage : CqControllerBase
+    {
+        public CqControllerSendGroupMessage(CqContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        private readonly CqContext _context;
+
+        public override JToken Process(JToken payload)
+        {
+            //_context.KhHost.Bot.SendMessage(
+            //    long.Parse(payload["group_id"]?.ToObject<string>()!),
+            //    payload["auto_escape"].ToObject<bool>()
+            //        ? payload["message"]?.ToObject<string>()
+            //        : _context.CqMessageHost.Parse(payload["message"]?.ToObject<string>()).ToString());
+
+            _context.KhHost.Bot.SendMessage(
+                long.Parse(payload["group_id"]?.ToObject<string>()!),
+                payload["message"]?.ToObject<string>());
+
+            return JToken.FromObject(new
+            {
+                message_id = 0
+            });
+        }
+    }
+
+    [CqController("send_msg")]
+    public class CqControllerSendMessage : CqControllerBase
+    {
+        public CqControllerSendMessage(CqContext context) : base(context)
+        {
+        }
+
+        public override JToken Process(JToken payload)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
