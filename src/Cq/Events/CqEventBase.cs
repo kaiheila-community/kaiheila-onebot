fb26099755fbeb7cebbcbbf18a3e45bf52874a09
Il Harper <afanyiyu@hotmail.com>
2020-12-01 14:48:56 +0800

    feat: Add CqEventAttribute

--- src/Cq/Events/CqEventBase.cs
+++ src/Cq/Events/CqEventBase.cs
@@ -1,6 +1,18 @@
-﻿namespace Kaiheila.Cqhttp.Cq.Events
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kaiheila.Cqhttp.Cq.Events
 {
     public class CqEventBase
     {
+        public Dictionary<string, string> Params = new Dictionary<string, string>();
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    sealed class CqEventAttribute : Attribute
+    {
+        public CqEventAttribute(Type type) => Type = type;
+
+        public readonly Type Type;
     }
 }
