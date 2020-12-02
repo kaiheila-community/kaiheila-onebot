b051213cdccaa4a6a3a60fe13d260f680bcb4fbd
Il Harper <afanyiyu@hotmail.com>
2020-12-02 21:18:47 +0800

    feat: Add CqDatabaseContext

---
+++ src/Cq/Database/CqDatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Kaiheila.Cqhttp.Storage;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kaiheila.Cqhttp.Cq.Database
+{
+    public sealed class CqDatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlite(@$"Data Source={StorageHelper.GetRootFilePath("database.db")}");
+    }
+}
