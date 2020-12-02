9ec92ada75813a593c312fe4701538202cf6ee0a
Il Harper <afanyiyu@hotmail.com>
2020-12-02 19:17:32 +0800

    test: Add CqEvents

---
+++ test/Cq/Events/CqEvents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Kaiheila.Cqhttp.Cq.Events;
+using Xunit;
+
+namespace Kaiheila.Cqhttp.Test.Cq.Events
+{
+    public static class CqEvents
+    {
+        [Fact]
+        public static void CqEventBasedOnCqEventBase()
+        {
+            foreach (Type type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x =>
+                x.GetTypes()
+                    .Where(type => Attribute.GetCustomAttribute(type, typeof(CqEventAttribute)) is not null)))
+                Assert.Equal(typeof(CqEventBase), type.BaseType);
+        }
+    }
+}
