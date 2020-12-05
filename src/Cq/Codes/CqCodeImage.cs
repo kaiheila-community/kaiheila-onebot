3689b269c269a8a0ac090bb82de67f93a5688a51
Il Harper <afanyiyu@hotmail.com>
2020-12-05 16:11:41 +0800

    feat: CqCodeImage Update

--- src/Cq/Codes/CqCodeImage.cs
+++ src/Cq/Codes/CqCodeImage.cs
@@ -1,4 +1,9 @@
-﻿namespace Kaiheila.OneBot.Cq.Codes
+﻿using System.Threading.Tasks;
+using Kaiheila.Events;
+using Kaiheila.OneBot.Cq.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kaiheila.OneBot.Cq.Codes
 {
     [CqCode("image")]
     public class CqCodeImage : CqCodeBase
@@ -20,6 +25,45 @@
 
         public readonly string Url;
 
-        public override string ConvertToString() => $"（图片：{Url} ）";
+        public override async Task<KhEventBase> ConvertToKhEvent(CqContext context, long channel)
+        {
+            CqDatabaseContext database = new CqDatabaseContext();
+
+            try
+            {
+                CqAsset asset = await database.Assets.SingleOrDefaultAsync(x => x.Url == Url);
+
+                if (asset is null)
+                {
+                    KhEventImage image = await context.KhHost.Bot.UploadImage(
+                        File,
+                        channel,
+                        Url);
+
+                    await database.Assets.AddAsync(new CqAsset
+                    {
+                        Path = image.Path,
+                        Url = Url,
+                        Name = File
+                    });
+
+                    await database.SaveChangesAsync();
+
+                    return image;
+                }
+                else
+                {
+                    return new KhEventImage(
+                        asset.Path,
+                        asset.Name);
+                }
+            }
+            finally
+            {
+                await database.DisposeAsync();
+            }
+        }
+
+        public override async Task<string> ConvertToString() => $"（图片：{Url} ）";
     }
 }
