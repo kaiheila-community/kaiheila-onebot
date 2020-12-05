dd1e5ebca1f174aa7c1843f331db5959014a4ff4
Il Harper <afanyiyu@hotmail.com>
2020-12-05 15:52:56 +0800

    feat: Add CqAsset Class

---
+++ src/Cq/Database/CqAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kaiheila.OneBot.Cq.Database
+{
+    public class CqAsset
+    {
+        [Key]
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Path { get; set; }
+    }
+}
