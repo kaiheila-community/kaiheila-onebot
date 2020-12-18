2b53805c1447811408e13efd03f47c40f75c98e1
Il Harper <afanyiyu@hotmail.com>
2020-12-19 01:32:46 +0800

    feat: Add AuthorizationMiddleware

--- src/Cq/AuthorizationHelper.cs
+++ src/Cq/AuthorizationHelper.cs
@@ -1,69 +1,87 @@
 ﻿using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using Kaiheila.OneBot.Storage;
 using Kaiheila.OneBot.Utils;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 
 namespace Kaiheila.OneBot.Cq
 {
-    public static class AuthorizationHelper
+    public class AuthorizationMiddleware
     {
         private const string AuthorizationHeader = "Authorization";
         private const string BearerPrefix = "Bearer ";
 
+        public AuthorizationMiddleware(
+            RequestDelegate next,
+            IOptions<ConfigHelper> options)
+        {
+            _next = next;
+            _options = options;
+        }
+
+        private RequestDelegate _next;
+        private readonly IOptions<ConfigHelper> _options;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Skip Authorization When IsNullOrEmpty
+            if (string.IsNullOrEmpty(_options.Value.Config.CqConfig.CqAuthConfig.AccessToken))
+            {
+                await _next(context);
+                return;
+            }
+
+            bool hasAccessTokenInHeader = context.Request.Headers
+                .TryGetValue(AuthorizationHeader, out StringValues authValue) && authValue.Any();
+
+            if (hasAccessTokenInHeader)
+            {
+                if (
+                    authValue.FirstOrDefault() ==
+                    BearerPrefix + _options.Value.Config.CqConfig.CqAuthConfig.AccessToken)
+                {
+                    await _next(context);
+                    return;
+                }
+
+                context.Response.SetStatusCode(HttpStatusCode.Forbidden);
+                return;
+            }
+
+            bool hasAccessTokenInQuery =
+                context.Request.Query.TryGetValue("access_token", out authValue) &&
+                authValue.Any();
+
+            if (hasAccessTokenInQuery)
+            {
+                if (
+                    authValue.FirstOrDefault() ==
+                    _options.Value.Config.CqConfig.CqAuthConfig.AccessToken)
+                {
+                    await _next(context);
+                    return;
+                }
+
+                context.Response.SetStatusCode(HttpStatusCode.Forbidden);
+                return;
+            }
+
+            context.Response.SetStatusCode(HttpStatusCode.Unauthorized);
+        }
+    }
+
+    public static class AuthorizationExtensions
+    {
         public static IApplicationBuilder UseCqAuthorization(
             this IApplicationBuilder builder,
             ConfigHelper configHelper)
         {
-            builder.Use(
-                next => async context =>
-                {
-                    // Skip Authorization When IsNullOrEmpty
-                    if (string.IsNullOrEmpty(configHelper.Config.CqConfig.CqAuthConfig.AccessToken))
-                    {
-                        await next(context);
-                        return;
-                    }
-
-                    bool hasAccessTokenInHeader = context.Request.Headers
-                        .TryGetValue(AuthorizationHeader, out StringValues authValue) && authValue.Any();
-
-                    if (hasAccessTokenInHeader)
-                    {
-                        if (
-                            authValue.FirstOrDefault() ==
-                            BearerPrefix + configHelper.Config.CqConfig.CqAuthConfig.AccessToken)
-                        {
-                            await next(context);
-                            return;
-                        }
-
-                        context.Response.SetStatusCode(HttpStatusCode.Forbidden);
-                        return;
-                    }
-
-                    bool hasAccessTokenInQuery =
-                        context.Request.Query.TryGetValue("access_token", out authValue) &&
-                        authValue.Any();
-
-                    if (hasAccessTokenInQuery)
-                    {
-                        if (
-                            authValue.FirstOrDefault() ==
-                            configHelper.Config.CqConfig.CqAuthConfig.AccessToken)
-                        {
-                            await next(context);
-                            return;
-                        }
-
-                        context.Response.SetStatusCode(HttpStatusCode.Forbidden);
-                        return;
-                    }
-
-                    context.Response.SetStatusCode(HttpStatusCode.Unauthorized);
-                });
-
+            builder.UseMiddleware<AuthorizationMiddleware>(
+                Options.Create(configHelper));
             return builder;
         }
     }
